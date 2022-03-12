cb7c8b2f0af29b2f93dc0704c4864cb954013c13
Hahahahx <1219654535@qq.com>
2022-03-12 21:10:04 +0800

    update:路由类型提示

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,34 +1,14 @@
 /// <reference types="react" />
 
-import { Routers, useRouter, RouterView, RouteParams } from "ux-autoroute";
-
-declare namespace Box {
-    /**
-     * routers 路由映射表对象
-     * noMatch 404
-     * before 访问路有前触发，如果结果返回了JSX对象的话则替换默认的路由组件
-     * after 路由组件生成后触发
-     */
-    interface RunConfig {
-        router: Route;
-    }
-
-    interface Route {
-        router: Array<RouteParams>;
-        isHashRouter?: boolean;
-        NoMatch: () => React.ReactElement | JSX.Element;
-    }
-}
+import {
+    Routers,
+    useRouter,
+    RouterView,
+    RouteParams,
+    RouterParams,
+} from "ux-autoroute";
 
 declare module "ux-box-min" {
-    /**
-     * 启动文件
-     * @param params  modules,router 
-     * @param params.modules  UserModule 
-     * @param params.router 
-    
-     */
-    function run(params: Box.RunConfig): void;
-
+    function run(params: RouterParams): void;
     export { run, Routers, RouterView, useRouter };
 }
